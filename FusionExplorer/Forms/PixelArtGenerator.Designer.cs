1632730a4dff2f3c81d21e8d3468202adbf65554
Jake Thornton <jt021101@gmail.com>
2025-03-06 22:12:42 +1000

    Created developmental feature branches and removed features from master

--- FusionExplorer/Forms/PixelArtGenerator.Designer.cs
+++ FusionExplorer/Forms/PixelArtGenerator.Designer.cs
@@ -230,7 +230,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 650);
+            this.ClientSize = new System.Drawing.Size(727, 516);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button5);
